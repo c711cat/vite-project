d62296366d32ae43b9022e129b7aa631cc07f886
c711cat <c711cat@gmail.com>
2024-10-19 16:07:55 +0800

    first push

---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    }
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT)
+    console.log(import.meta.env.VITE_PATH)
+    // const url = import.meta.env.VITE_PATH
+  },
+}
+</script>
